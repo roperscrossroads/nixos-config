7737c7a9ba61a09511f6b27c156b1bbbe79f5486
Adam Roper <kyushuadamu@protonmail.com>
2024-12-17 18:42:16 -0500

    feat: add proxmox!

--- flake.nix
+++ flake.nix
@@ -1,22 +1,31 @@
 {
-  description = "Nixos config flake";
+  description = "Nixos config flake with Proxmox support";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    proxmox-nixos.url = "github:SaumonNet/proxmox-nixos";  # Add Proxmox-NixOS input
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, proxmox-nixos, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
+      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
-        # inputs.home-manager.nixosModules.default
+        proxmox-nixos.nixosModules.proxmox-ve  # Add Proxmox VE module
+        ({ pkgs, lib, ... }: {
+          services.proxmox-ve = {
+            enable = true;
+            ipAddress = "10.0.0.52";  # Set your desired Proxmox IP address
+          };
+
+          nixpkgs.overlays = [
+            proxmox-nixos.overlays.x86_64-linux  # Apply the Proxmox overlay
+          ];
+
+          # You can add other configuration settings as needed here
+        })
       ];
     };
   };
 }
+
